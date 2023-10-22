4c0af9b1f235e63918e4f4dc741fe94dd8d6acce
SheikhSuhailKhursheed <sheikhsuhail2026@gmail.com>
2023-10-22 20:58:40 +0530

    Updated project

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Sorting-Visualizer
 
-### This is a simple visualization project made using javascript 
-- Bubble Sort 
+The Sorting Visualizer is an interactive project created with JavaScript that allows you to visualize the working of various sorting algorithms used in computer science. It's a valuable educational tool for understanding and comparing different sorting techniques.
+
+### The project includes the following sorting algorithms:
+
+- Bubble Sort
 - Selection Sort
 - Insertion Sort
 - Quick Sort
 - Merge Sort
 
-### This is built using HTML, CSS, JavaScript <br/>
+### How to Use:
 
-[Check out the website here](https://sheikhsuhail19.github.io/Sorting-Visualizer/)
+To use the Sorting Visualizer, simply open the HTML file in your web browser. You can choose from the available sorting algorithms and see them in action. This project serves as an excellent resource for learning and teaching sorting algorithms.
+
+<!-- [Check out the website here](https://sheikhsuhail19.github.io/Sorting-Visualizer/) -->
+
+This project not only aids in understanding sorting algorithms but also provides an engaging and interactive learning experience. Explore the sorting process and gain insights into how these algorithms work.
+
+### Working images of project
 
 <img src="img/image1.png"> <br/>
 <img src="img/image2.png"> <br/>
--- js_files/insertion.js
+++ js_files/insertion.js
@@ -2,38 +2,38 @@ async function insertion() {
     console.log('In insertion()');
     const ele = document.querySelectorAll(".bar");
     // color
-    ele[0].style.background = 'green';
-    for(let i = 1; i < ele.length; i++) {
+    ele[ 0 ].style.background = 'green';
+    for (let i = 1; i < ele.length; i++) {
         console.log('In ith loop');
         let j = i - 1;
-        let key = ele[i].style.height;
+        let key = ele[ i ].style.height;
         // color
-        ele[i].style.background = 'blue';
+        ele[ i ].style.background = 'blue';
 
         await waitforme(delay);
 
-        while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))) {
+        while (j >= 0 && (parseInt(ele[ j ].style.height) > parseInt(key))) {
             console.log('In while loop');
             // color
-            ele[j].style.background = 'blue';
-            ele[j + 1].style.height = ele[j].style.height;
+            ele[ j ].style.background = 'blue';
+            ele[ j + 1 ].style.height = ele[ j ].style.height;
             j--;
 
             await waitforme(delay);
 
             // color
-            for(let k = i; k >= 0; k--) {
-                ele[k].style.background = 'green';
+            for (let k = i; k >= 0; k--) {
+                ele[ k ].style.background = 'green';
             }
         }
-        ele[j + 1].style.height = key;
+        ele[ j + 1 ].style.height = key;
         // color
-        ele[i].style.background = 'green';
+        ele[ i ].style.background = 'green';
     }
 }
 
 const inSortbtn = document.querySelector(".insertionSort");
-inSortbtn.addEventListener('click', async function(){
+inSortbtn.addEventListener('click', async function () {
     disableSortingBtn();
     disableSizeSlider();
     disableNewArrayBtn();
